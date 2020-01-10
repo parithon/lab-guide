7fddc5fca17fdc000be5f11d3b2c2f5fc0af1b4a
Anthony Conrad (parithon) <parithon@outlook.com>
2020-01-09 23:54:42 -0800

    Tailwind CSS added (#1)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,16 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 
-export default () => <div>Hello world!</div>
+import "../styles/global.css"
+
+export default () => {
+  return (
+    <>
+      <Helmet>
+        <html en="lang" />
+        <body className="bg-gray-100 font-sans leading-normal tracking-normal" />
+      </Helmet>
+      <div>Hello world!</div>
+    </>
+  )
+}
